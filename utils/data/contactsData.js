47aee2ed178715d3b7d355f537b46e2c6f53ce47
rodtomi <tomi.rodriguezv@gmail.com>
2024-12-31 14:24:55 -0300

    Attempt 1 ...

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'tomi.rodriguezv@gmail.com',
+    phone: '+54 9 11 5862-0307',
+    address: 'Martinez, Buenos Aires, Argentina',
+    github: 'https://github.com/rodtomi',
+    facebook: 'https://www.facebook.com/tomi.rodriguez.73/',
+    linkedIn: 'https://www.linkedin.com/in/tomas-rodriguez-51661162/',
+    twitter: '',
+    stackOverflow: '',
+    devUsername: ''
 }
